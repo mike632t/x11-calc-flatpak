14a193de6ad9dbfca998dfb1f8ace1cf47e757fd
macmpi <16296055+macmpi@users.noreply.github.com>
2024-02-17 17:47:21 +0100

    add x11-calc subdir

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ It can also be launched under terminal (to monitor eventual error messages):\
 User can launch any of the project calculator simulator. Preferred calculator model reference can be set by running in terminal:\
 `flatpak run io.github.mike632t.x11_calc --setup`\
 or similarly editing:\
-`nano ~/.var/app/io.github.mike632t.x11_calc/config/x11-calc.conf`\
+`nano ~/.var/app/io.github.mike632t.x11_calc/config/x11-calc/x11-calc.conf`\
 (default is `MODEL=35`)
 
 Voyager models (10c, 11c, 12c, 15c, 16c) can be used if relevant rom file is supplied at:\
-`~/.var/app/io.github.mike632t.x11_calc/data/$MODEL.rom`
+`~/.var/app/io.github.mike632t.x11_calc/data/x11-calc/$MODEL.rom`
 
 ## Building & testing
 Install flatpak-builder.
--- x11-calc-launcher.sh
+++ x11-calc-launcher.sh
@@ -5,14 +5,14 @@ OPTS=""
 
 _launch() {
 # shellcheck disable=SC1090  # intended include
-. "${XDG_CONFIG_HOME}"/x11-calc.conf
+. "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
 [ -z "$MODEL" ] && exit 1
 
 case $MODEL in
 	10c|11c|12c|15c|16c)
 		# if OPTS does not point to a rom file, set expected option to default location
 		# no need to check file existence: app will error-out with proper message if missing
-		[ -n "${OPTS##*.rom*}" ] || [ -z "${OPTS}" ] && OPTS="-r ${XDG_DATA_HOME}/${MODEL}.rom"
+		[ -n "${OPTS##*.rom*}" ] || [ -z "${OPTS}" ] && OPTS="-r ${XDG_DATA_HOME}/x11-calc/${MODEL}.rom"
 	;;
 esac
 
@@ -21,12 +21,13 @@ exec /app/bin/x11-calc-${MODEL} ${OPTS}
 }
 
 _setup() {
-nano "${XDG_CONFIG_HOME}"/x11-calc.conf
+nano "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
 }
 
 
-if ! [ -f "${XDG_CONFIG_HOME}"/x11-calc.conf ]; then
-	cat <<-EOF >"${XDG_CONFIG_HOME}"/x11-calc.conf
+if ! [ -f "${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf ]; then
+	mkdir -p "${XDG_CONFIG_HOME}"/x11-calc
+	cat <<-EOF >"${XDG_CONFIG_HOME}"/x11-calc/x11-calc.conf
 		MODEL=35
 		OPTS=""
 		EOF
